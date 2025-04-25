d1b3d5c8d7a275f82a615c509d2d7da0f7c5bbef
bhernardo17 <bhernardorv12@gmail.com>
2025-04-25 13:34:12 -0300

    Melhorias Gerais

--- src/config/email.js
+++ src/config/email.js
@@ -1,20 +1,14 @@
 import emailjs from '@emailjs/browser';
 
-// Configuração do EmailJS
-const PUBLIC_KEY = 'wUoEcPrO0T7mwJgDw';
-const SERVICE_ID = 'service_esp71zd';
-const TEMPLATE_ID = 'template_bfg4352';
-
-// Inicializa o EmailJS
-emailjs.init(PUBLIC_KEY);
+emailjs.init('u1Z_NDidNdO1TjhAI');
 
 export const sendEmail = async (templateParams) => {
   try {
     const response = await emailjs.send(
-      SERVICE_ID,
-      TEMPLATE_ID,
+      'service_b7gh90j', // Service ID do Gmail
+      'template_1mybwc8', // Template ID
       templateParams,
-      PUBLIC_KEY
+      'u1Z_NDidNdO1TjhAI' // Public Key
     );
     return { success: true, data: response };
   } catch (error) {
